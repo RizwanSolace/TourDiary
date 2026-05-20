85a33f274b8fc8cd6ceef16c76809e6f64a6fb10
RizwanSolace <rizwansolace31@gmail.com>
2026-05-20 18:44:08 +0530

    Update App.jsx

--- tourism-diary-frontend/src/App.jsx
+++ tourism-diary-frontend/src/App.jsx
@@ -6,7 +6,7 @@ import EntryForm from './components/EntryForm';
 import AllEntries from './components/AllEntries';
 import AddDestinationForm from './components/AddDestinationForm';
 
-const API_BASE = import.meta.env.API;
+const API_BASE = import.meta.env.VITE_API;
 
 function App() {
   const [view, setView] = useState('destinations');
